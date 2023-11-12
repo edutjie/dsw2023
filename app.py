b4b9e3ad7ccabce53fe7bd2f918fce2e09b36afc
edutjie <edutjitrahardja@gmail.com>
2023-11-12 16:31:05 +0700

    feat: implement location visualization

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import numpy as np
 import os
 import pickle
 import warnings
+import folium
+from folium.plugins import MarkerCluster
+from streamlit_folium import st_folium
 
 
 st.set_page_config(
@@ -16,7 +19,6 @@ st.set_page_config(
 
 def load_model(modelfile):
     loaded_model = pickle.load(open(modelfile, "rb"))
-
     return loaded_model
 
 
@@ -24,7 +26,6 @@ def preprocessing(df):
     df = df.copy()
     df.loc[df["Games Product"] != "No internet service", "internet_service"] = 1
     df["internet_service"].fillna(0, inplace=True)
-    df["Location"] = df["Location"].map({"Jakarta": 1, "Bandung": 0})
     df["Device Class"] = df["Device Class"].map(
         {"High End": 2, "Mid End": 1, "Low End": 0}
     )
@@ -57,106 +58,95 @@ def main():
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
+    df = pd.read_excel("data/Telco_customer_churn_adapted_v2.xlsx", sheet_name="Sheet1")
 
     tab1, tab2 = st.tabs(["Predict", "Analysis"])
 
     with tab1:
         col1, col2 = st.columns([2, 2])
         with col1:
-                st.subheader(" Find out if your customer will churn or not 🫥")
-                # Categorical columns unique values
-                location_values = ["Jakarta", "Bandung"]
-                device_class_values = ["Low End", "Mid End", "High End"]
-                games_product_values = ["Yes", "No", "No internet service"]
-                music_product_values = ["Yes", "No", "No internet service"]
-                education_product_values = ["Yes", "No", "No internet service"]
-                call_center_values = ["Yes", "No"]
-                video_product_values = ["Yes", "No", "No internet service"]
-                use_myapp_values = ["Yes", "No", "No internet service"]
-                payment_method_values = ["Digital Wallet", "Pulsa", "Debit", "Credit"]
+            st.subheader(" Find out if your customer will churn or not 🫥")
+            # Categorical columns unique values
+            device_class_values = ["Low End", "Mid End", "High End"]
+            games_product_values = ["Yes", "No", "No internet service"]
+            music_product_values = ["Yes", "No", "No internet service"]
+            education_product_values = ["Yes", "No", "No internet service"]
+            call_center_values = ["Yes", "No"]
+            video_product_values = ["Yes", "No", "No internet service"]
+            use_myapp_values = ["Yes", "No", "No internet service"]
+            payment_method_values = ["Digital Wallet", "Pulsa", "Debit", "Credit"]
 
-                # Create input fields for categorical columns
-                location = st.selectbox("Location", location_values)
-                device_class = st.selectbox("Device Class", device_class_values)
-                games_product = st.selectbox("Games Product", games_product_values)
-                music_product = st.selectbox("Music Product", music_product_values)
-                education_product = st.selectbox("Education Product", education_product_values)
-                call_center = st.selectbox("Call Center", call_center_values)
-                video_product = st.selectbox("Video Product", video_product_values)
-                use_myapp = st.selectbox("Use MyApp", use_myapp_values)
-                payment_method = st.selectbox("Payment Method", payment_method_values)
+            # Create input fields for categorical columns
+            device_class = st.selectbox("Device Class", device_class_values)
+            games_product = st.selectbox("Games Product", games_product_values)
+            music_product = st.selectbox("Music Product", music_product_values)
+            education_product = st.selectbox(
+                "Education Product", education_product_values
+            )
+            call_center = st.selectbox("Call Center", call_center_values)
+            video_product = st.selectbox("Video Product", video_product_values)
+            use_myapp = st.selectbox("Use MyApp", use_myapp_values)
+            payment_method = st.selectbox("Payment Method", payment_method_values)
 
-                # Create input fields for numerical columns
-                # 0 to inf
-                tenure_months = st.number_input("Tenure Months", 0)
-                monthly_purchase = st.number_input("Monthly Purchase (Thou. IDR)", 0)
-                longitude = st.number_input("Longitude", 0.0, 100000.0)
-                latitude = st.number_input("Latitude", 0.0, 100000.0)
-                cltv = st.number_input("CLTV (Predicted Thou. IDR)", 0)
+            # Create input fields for numerical columns
+            # 0 to inf
+            tenure_months = st.number_input("Tenure Months", 0)
+            monthly_purchase = st.number_input("Monthly Purchase (Thou. IDR)", 0)
+            cltv = st.number_input("CLTV (Predicted Thou. IDR)", 0)
 
-                # Combine input values into a feature list
-                feature_list = [
-                    tenure_months,
-                    monthly_purchase,
-                    longitude,
-                    latitude,
-                    cltv,
-                    location,
-                    device_class,
-                    games_product,
-                    music_product,
-                    education_product,
-                    call_center,
-                    video_product,
-                    use_myapp,
-                    payment_method,
+            # Combine input values into a feature list
+            feature_list = [
+                tenure_months,
+                monthly_purchase,
+                cltv,
+                device_class,
+                games_product,
+                music_product,
+                education_product,
+                call_center,
+                video_product,
+                use_myapp,
+                payment_method,
+            ]
+            # single_pred = np.array(feature_list).reshape(1,-1)
+            feat_df = pd.DataFrame(
+                [feature_list],
+                columns=[
+                    "Tenure Months",
+                    "Monthly Purchase (Thou. IDR)",
+                    "CLTV (Predicted Thou. IDR)",
+                    "Device Class",
+                    "Games Product",
+                    "Music Product",
+                    "Education Product",
+                    "Call Center",
+                    "Video Product",
+                    "Use MyApp",
+                    "Payment Method",
+                ],
+            )
+            single_pred = preprocessing(feat_df)
+            single_pred = single_pred[
+                [
+                    "Tenure Months",
+                    "Device Class",
+                    "Games Product",
+                    "Music Product",
+                    "Education Product",
+                    "Call Center",
+                    "Video Product",
+                    "Use MyApp",
+                    "Monthly Purchase (Thou. IDR)",
+                    "CLTV (Predicted Thou. IDR)",
+                    "internet_service",
+                    "pm_Credit",
+                    "pm_Debit",
+                    "pm_Digital Wallet",
+                    "pm_Pulsa",
                 ]
-                # single_pred = np.array(feature_list).reshape(1,-1)
-                feat_df = pd.DataFrame(
-                    [feature_list],
-                    columns=[
-                        "Tenure Months",
-                        "Monthly Purchase (Thou. IDR)",
-                        "Longitude",
-                        "Latitude",
-                        "CLTV (Predicted Thou. IDR)",
-                        "Location",
-                        "Device Class",
-                        "Games Product",
-                        "Music Product",
-                        "Education Product",
-                        "Call Center",
-                        "Video Product",
-                        "Use MyApp",
-                        "Payment Method",
-                    ],
-                )
-                single_pred = preprocessing(feat_df)
-                single_pred = single_pred[
-                    [
-                        "Tenure Months",
-                        "Location",
-                        "Device Class",
-                        "Games Product",
-                        "Music Product",
-                        "Education Product",
-                        "Call Center",
-                        "Video Product",
-                        "Use MyApp",
-                        "Monthly Purchase (Thou. IDR)",
-                        "Longitude",
-                        "Latitude",
-                        "CLTV (Predicted Thou. IDR)",
-                        "internet_service",
-                        "pm_Credit",
-                        "pm_Debit",
-                        "pm_Digital Wallet",
-                        "pm_Pulsa",
-                    ]
-                ]
+            ]
 
-                predict_button = st.button("Predict")
-
+            predict_button = st.button("Predict")
 
         with col2:
             with st.expander(" ℹ️ Information", expanded=True):
@@ -170,16 +160,18 @@ def main():
             ## How does it work ❓ 
             Complete all the parameters and the machine learning model will predict the probability that the customer will churn.
             """
-            
+
             if predict_button:
-                loaded_model = load_model("models/baseline_model.pkl")
+                loaded_model = load_model("models/best_model.pkl")
                 prediction = loaded_model.predict_proba(single_pred)
                 col2.write(
                     """
                 ## Results 🔍 
                 """
                 )
-                col2.success(f"The probability that the customer will churn is {round(prediction[0, 1] * 100, 2)}%")
+                col2.success(
+                    f"The probability that the customer will churn is {round(prediction[0, 1] * 100, 2)}%"
+                )
 
     with tab2:
         st.subheader("Analysis")
@@ -189,11 +181,23 @@ def main():
             """
         )
 
+        st.subheader("Location Visualization")
+        col1, col2 = st.columns([2, 2])
+        with col1:
+            with open("maps/map.html", "r") as html_file:
+                folium_html_content = html_file.read()
+            st.components.v1.html(folium_html_content, height=400)
+
+        with col2:
+            with open("maps/geo_map.html", "r") as html_file:
+                folium_html_content2 = html_file.read()
+            st.components.v1.html(folium_html_content2, height=400)
+
+        # st_folium(m, width=725)
+
     # code for html ☘️ 🌾 🌳 👨‍🌾  🍃
 
-    st.warning(
-        "Note: Lorem ipsum [here](https://github.com/edutjie/dsw2023)"
-    )
+    st.warning("Note: Lorem ipsum [here](https://github.com/edutjie/dsw2023)")
     hide_menu_style = """
     <style>
     #MainMenu {visibility: hidden;}
